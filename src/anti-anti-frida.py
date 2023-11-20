2ffb2f6c00d54f2ccb8d71491dc1c23e23a7f4d4
Axojhf <20625305+axojhf@users.noreply.github.com>
2023-11-20 15:11:18 +0800

    fix

--- src/anti-anti-frida.py
+++ src/anti-anti-frida.py
@@ -24,6 +24,18 @@ if __name__ == "__main__":
         if "FRIDA" in symbol.name:
             symbol.name = symbol.name.replace("FRIDA", random_name)
 
+    all_patch_string = ["FridaScriptEngine", "GLib-GIO", "GDBusProxy", "GumScript"]  # 字符串特征修改 尽量与源字符一样
+    for section in binary.sections:
+        print(section.name)
+        if section.name != ".rodata":
+            continue
+        for patch_str in all_patch_string:
+            addr_all = section.search_all(patch_str)  # Patch 内存字符串
+            for addr in addr_all:
+                patch = [ord(n) for n in list(patch_str)[::-1]]
+                print(f"current section name={section.name} offset={hex(section.file_offset + addr)} {patch_str}-{''.join(list(patch_str)[::-1])}")
+                binary.patch_address(section.file_offset + addr, patch)
+
     binary.write(input_file)
     # gum-js-loop thread
     random_name = "".join(random.sample("abcdefghijklmn", 11))
@@ -33,3 +45,7 @@ if __name__ == "__main__":
     random_name = "".join(random.sample("abcdefghijklmn", 5))
     print(f"[*] Patch `gmain` to `{random_name}`")
     os.system(f"sed -b -i s/gmain/{random_name}/g {input_file}")
+    # thread_gdbus
+    random_name = "".join(random.sample("abcdefghijklmn", 5))
+    print(f"[*] Patch `gdbus` to `{random_name}`")
+    os.system(f"sed -b -i s/gdbus/{random_name}/g {input_file}")
